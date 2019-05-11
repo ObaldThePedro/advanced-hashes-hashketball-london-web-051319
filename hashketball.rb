a74c2580635d995c22dd42adf97fea4cf603e1d0
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-05-11 01:40:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,7 +176,7 @@ end
 def player_numbers(team_name)
   game_hash.each do |team, team_details|
     if team_details[:team_name] == team_name
-      team_details[:players]
+      team_details[:players].map
     end
   end
 end
